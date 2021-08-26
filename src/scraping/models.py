13f94328d029231b178ef2fccdc1104f2d764fd6
Andreyr76 <rabotbiz@gmail.com>
2021-08-26 22:28:00 +0300

    second

---
+++ src/scraping/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class city(models.Model):
+    name = models.CharField(max_length=50,
+    verbose_name = 'Название населенного пункта')
+    slug = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        verbose_name = 'Название населенного пункта'
+        verbose_name_plural = 'Название населенных пунктов'
+
+    def __str__(self):
+        return self.name
